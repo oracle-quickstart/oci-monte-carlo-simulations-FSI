c92043fcef45bfae58d806bb576410031dae15ab
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-09-09 16:54:40 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#git clone https://github.com/oracle-quickstart/oci-monte-carlo-simulations-FSI.git
+
+./kubectl create -f ks8-deployment/rabbitmq-controller.yaml
+./kubectl create -f ks8-deployment/rabbitmq-service.yaml
+./kubectl create -f ks8-deployment/loadbalancer-service.yaml
+./kubectl create -f ks8-deployment/mcv-controller.yaml
+./kubectl create -f ks8-deployment/mcv-client-controller.yaml
+./kubectl create -f ks8-deployment/mcv-parent-controller.yaml
+
+#./kubectl create -f oci-monte-carlo-simulations-FSI/ks8-deployment/splunk-controller.yaml
+#./kubectl create -f oci-monte-carlo-simulations-FSI/ks8-deployment/splunk-service.yaml
