d139701896c7cb55625cafaa6d2d0c1efe352bd5
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-09-09 17:35:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-./kubectl create -f ks8-deployment/rabbitmq-controller.yaml
-./kubectl create -f ks8-deployment/rabbitmq-service.yaml
-./kubectl create -f ks8-deployment/loadbalancer-service.yaml
-./kubectl create -f ks8-deployment/mcv-controller.yaml
-./kubectl create -f ks8-deployment/mcv-client-controller.yaml
-./kubectl create -f ks8-deployment/mcv-parent-controller.yaml
+./../kubectl create -f ks8-deployment/rabbitmq-controller.yaml
+./../kubectl create -f ks8-deployment/rabbitmq-service.yaml
+./../kubectl create -f ks8-deployment/loadbalancer-service.yaml
+sleep 15
+./../kubectl create -f ks8-deployment/mcv-controller.yaml
+./../kubectl create -f ks8-deployment/mcv-client-controller.yaml
+./../kubectl create -f ks8-deployment/mcv-parent-controller.yaml
