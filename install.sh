203dcbf2c44b520345652093d05a8fb4af64bebd
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-09-10 17:54:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
+./../kubectl create -f ks8-deployment/storageClass.yaml
+./../kubectl create -f ks8-deployment/persistentVolume.yaml
+./../kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
+./../kubectl create -f ks8-deployment/splunk-controller.yaml
+./../kubectl create -f ks8-deployment/splunk-service.yaml
+echo "Reporting tool deployed"
+
 ./../kubectl create -f ks8-deployment/rabbitmq-controller.yaml
 ./../kubectl create -f ks8-deployment/rabbitmq-service.yaml
 ./../kubectl create -f ks8-deployment/loadbalancer-service.yaml
 sleep 15
+
 ./../kubectl create -f ks8-deployment/mcv-controller.yaml
 ./../kubectl create -f ks8-deployment/mcv-client-controller.yaml
 ./../kubectl create -f ks8-deployment/mcv-parent-controller.yaml
+
+
+
+
+
