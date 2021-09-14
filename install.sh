be44ecab0a84ff15fdbb191f6eda567f5ff457a5
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-09-14 17:52:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo "Deploying OCI FSI Monte Carlo Simulator"
+
 ./../kubectl create -f ks8-deployment/storageClass.yaml
 ./../kubectl create -f ks8-deployment/persistentVolume.yaml
 ./../kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
@@ -7,13 +9,15 @@ echo "Reporting tool deployed"
 
 ./../kubectl create -f ks8-deployment/rabbitmq-controller.yaml
 ./../kubectl create -f ks8-deployment/rabbitmq-service.yaml
+echo "RabbitMQ deployed"
 ./../kubectl create -f ks8-deployment/loadbalancer-service.yaml
+echo "LoadBalancer deployed"
 sleep 15
 
 ./../kubectl create -f ks8-deployment/mcv-controller.yaml
-./../kubectl create -f ks8-deployment/mcv-client-controller.yaml
 ./../kubectl create -f ks8-deployment/mcv-parent-controller.yaml
-
+./../kubectl create -f ks8-deployment/mcv-client-controller.yaml
+echo "Monte Carlo Simulator core deployed"
 
 
 
