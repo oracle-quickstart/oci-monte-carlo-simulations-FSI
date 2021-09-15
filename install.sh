1ff47d94be41c663624b18078977cfa05d4e9976
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-09-15 17:22:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 echo "Deploying OCI FSI Monte Carlo Simulator"
 
-./../kubectl create -f ks8-deployment/storageClass.yaml
-./../kubectl create -f ks8-deployment/persistentVolume.yaml
-./../kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
-./../kubectl create -f ks8-deployment/splunk-controller.yaml
-./../kubectl create -f ks8-deployment/splunk-service.yaml
+kubectl create -f ks8-deployment/storageClass.yaml
+kubectl create -f ks8-deployment/persistentVolume.yaml
+kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
+kubectl create -f ks8-deployment/splunk-controller.yaml
+kubectl create -f ks8-deployment/splunk-service.yaml
 echo "Reporting tool deployed"
 
-./../kubectl create -f ks8-deployment/rabbitmq-controller.yaml
-./../kubectl create -f ks8-deployment/rabbitmq-service.yaml
+kubectl create -f ks8-deployment/rabbitmq-controller.yaml
+kubectl create -f ks8-deployment/rabbitmq-service.yaml
 echo "RabbitMQ deployed"
-./../kubectl create -f ks8-deployment/loadbalancer-service.yaml
+kubectl create -f ks8-deployment/loadbalancer-service.yaml
 echo "LoadBalancer deployed"
 sleep 15
 
-./../kubectl create -f ks8-deployment/mcv-controller.yaml
-./../kubectl create -f ks8-deployment/mcv-parent-controller.yaml
-./../kubectl create -f ks8-deployment/mcv-client-controller.yaml
+kubectl create -f ks8-deployment/mcv-controller.yaml
+kubectl create -f ks8-deployment/mcv-parent-controller.yaml
+kubectl create -f ks8-deployment/mcv-client-controller.yaml
 echo "Monte Carlo Simulator core deployed"
 
 
