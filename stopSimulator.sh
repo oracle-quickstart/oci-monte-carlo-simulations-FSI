9a57e0c778d698d77fd6b7f4d40ee11f5796d5d9
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-11-08 19:37:03 +0100

    Update stopSimulator.sh

--- stopSimulator.sh
+++ stopSimulator.sh
@@ -1,8 +1,17 @@
-kubectl delete replicationcontroller rabbitmq-controller
-kubectl delete replicationcontroller mcv-controller
-kubectl delete replicationcontroller mcv-parent-controller
-kubectl delete replicationcontroller mcv-client-controller
-kubectl delete replicationcontroller splunk-controller
-kubectl delete service loadbalancer
-kubectl delete service rabbitmq-service
-kubectl delete service splunk-service
+#!/bin/bash
+
+if [ $1 == "light" ]
+then
+  kubectl delete replicationcontroller mcv-controller
+  kubectl delete replicationcontroller mcv-parent-controller
+  kubectl delete replicationcontroller mcv-client-controller
+else
+  kubectl delete replicationcontroller rabbitmq-controller
+  kubectl delete replicationcontroller mcv-controller
+  kubectl delete replicationcontroller mcv-parent-controller
+  kubectl delete replicationcontroller mcv-client-controller
+  kubectl delete replicationcontroller splunk-controller
+  kubectl delete service loadbalancer
+  kubectl delete service rabbitmq-service
+  kubectl delete service splunk-service
+fi
