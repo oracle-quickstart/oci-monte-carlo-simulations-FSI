a02e40f763a00e24a3fe6fae3bc78e2fa351069b
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-11-08 19:33:55 +0100

    Update startSimulator.sh

--- startSimulator.sh
+++ startSimulator.sh
@@ -1,22 +1,33 @@
+#!/bin/bash
+
+var = ""
+
+if [ $1 == "all" ]
+then
+  var = "all_architectures"
+else
+  var = "intel"
+fi
+
 echo "Deploying OCI FSI Monte Carlo Simulator"
 
-kubectl create -f ks8-deployment/storageClass.yaml
-kubectl create -f ks8-deployment/persistentVolume.yaml
-kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
-kubectl create -f ks8-deployment/splunk-controller.yaml
-kubectl create -f ks8-deployment/splunk-service.yaml
+kubectl create -f ks8-deployment/$var/storageClass.yaml
+kubectl create -f ks8-deployment/$var/persistentVolume.yaml
+kubectl create -f ks8-deployment/$var/persistentVolumeClaim.yaml
+kubectl create -f ks8-deployment/$var/splunk-controller.yaml
+kubectl create -f ks8-deployment/$var/splunk-service.yaml
 echo "Reporting tool deployed"
 
-kubectl create -f ks8-deployment/rabbitmq-controller.yaml
-kubectl create -f ks8-deployment/rabbitmq-service.yaml
+kubectl create -f ks8-deployment/$var/rabbitmq-controller.yaml
+kubectl create -f ks8-deployment/$var/rabbitmq-service.yaml
 echo "RabbitMQ deployed"
-kubectl create -f ks8-deployment/loadbalancer-service.yaml
+kubectl create -f ks8-deployment/$var/loadbalancer-service.yaml
 echo "LoadBalancer deployed"
 sleep 15
 
-kubectl create -f ks8-deployment/mcv-controller.yaml
-kubectl create -f ks8-deployment/mcv-parent-controller.yaml
-kubectl create -f ks8-deployment/mcv-client-controller.yaml
+kubectl create -f ks8-deployment/$var/mcv-controller.yaml
+kubectl create -f ks8-deployment/$var/mcv-parent-controller.yaml
+kubectl create -f ks8-deployment/$var/mcv-client-controller.yaml
 echo "Monte Carlo Simulator core deployed"
 
 
