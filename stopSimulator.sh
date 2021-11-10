37c953126378f2b6d73c2e53a39bc56fdb4dcc4a
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-11-10 09:59:15 +0100

    Update stopSimulator.sh

--- stopSimulator.sh
+++ stopSimulator.sh
@@ -6,12 +6,17 @@ then
   kubectl delete replicationcontroller mcv-parent-controller
   kubectl delete replicationcontroller mcv-client-controller
 else
-  kubectl delete replicationcontroller rabbitmq-controller
-  kubectl delete replicationcontroller mcv-controller
-  kubectl delete replicationcontroller mcv-parent-controller
-  kubectl delete replicationcontroller mcv-client-controller
-  kubectl delete replicationcontroller splunk-controller
-  kubectl delete service loadbalancer
-  kubectl delete service rabbitmq-service
-  kubectl delete service splunk-service
+  if [ "$1" == "all" ]
+  then
+    kubectl delete replicationcontroller rabbitmq-controller
+    kubectl delete replicationcontroller mcv-controller
+    kubectl delete replicationcontroller mcv-parent-controller
+    kubectl delete replicationcontroller mcv-client-controller
+    kubectl delete replicationcontroller splunk-controller
+    kubectl delete service loadbalancer
+    kubectl delete service rabbitmq-service
+    kubectl delete service splunk-service
+  else
+    echo "Please, follow the instructions and include light or all to select the stop mode"
+  fi
 fi
