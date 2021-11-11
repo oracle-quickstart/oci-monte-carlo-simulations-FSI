c84f17114d0e3cf0960e02ad637bd114a5cecde4
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-11-11 12:31:30 +0100

    Update startSimulator.sh

--- startSimulator.sh
+++ startSimulator.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-var=""
+#var=""
 
-if [ "$1" == "all" ]
-then
-  var="all_architectures"
-fi
+#if [ "$1" == "all" ]
+#then
+#  var="all_architectures"
+#fi
 
 echo "Deploying OCI FSI Monte Carlo Simulator"
 
-kubectl create -f ks8-deployment/$var/storageClass.yaml
-kubectl create -f ks8-deployment/$var/persistentVolume.yaml
-kubectl create -f ks8-deployment/$var/persistentVolumeClaim.yaml
-kubectl create -f ks8-deployment/$var/splunk-controller.yaml
-kubectl create -f ks8-deployment/$var/splunk-service.yaml
+kubectl create -f ks8-deployment/all_architectures/storageClass.yaml
+kubectl create -f ks8-deployment/all_architectures/persistentVolume.yaml
+kubectl create -f ks8-deployment/all_architectures/persistentVolumeClaim.yaml
+kubectl create -f ks8-deployment/all_architectures/splunk-controller.yaml
+kubectl create -f ks8-deployment/all_architectures/splunk-service.yaml
 echo "Reporting tool deployed"
 
-kubectl create -f ks8-deployment/$var/rabbitmq-controller.yaml
-kubectl create -f ks8-deployment/$var/rabbitmq-service.yaml
+kubectl create -f ks8-deployment/all_architectures/rabbitmq-controller.yaml
+kubectl create -f ks8-deployment/all_architectures/rabbitmq-service.yaml
 echo "RabbitMQ deployed"
-kubectl create -f ks8-deployment/$var/loadbalancer-service.yaml
+kubectl create -f ks8-deployment/all_architectures/loadbalancer-service.yaml
 echo "LoadBalancer deployed"
 sleep 15
 
-kubectl create -f ks8-deployment/$var/mcv-controller.yaml
-kubectl create -f ks8-deployment/$var/mcv-parent-controller.yaml
-kubectl create -f ks8-deployment/$var/mcv-client-controller.yaml
+kubectl create -f ks8-deployment/all_architectures/mcv-controller.yaml
+kubectl create -f ks8-deployment/all_architectures/mcv-parent-controller.yaml
+kubectl create -f ks8-deployment/all_architectures/mcv-client-controller.yaml
 echo "Monte Carlo Simulator core deployed"
