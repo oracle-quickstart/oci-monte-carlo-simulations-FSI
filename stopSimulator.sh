b0fe5cee9f5f1a9c3d06a635bef95cbb54ade335
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2021-11-16 18:53:18 +0100

    Update stopSimulator.sh

--- stopSimulator.sh
+++ stopSimulator.sh
@@ -2,16 +2,22 @@
 
 if [ "$1" == "light" ]
 then
-  kubectl delete replicationcontroller mcv-controller
-  kubectl delete replicationcontroller mcv-parent-controller
-  kubectl delete replicationcontroller mcv-client-controller
+  kubectl delete replicationcontroller intel-worker-controller
+  kubectl delete replicationcontroller intel-splitter-controller
+  kubectl delete replicationcontroller intel-client-controller
+  kubectl delete replicationcontroller arm-worker-controller
+  kubectl delete replicationcontroller arm-splitter-controller
+  kubectl delete replicationcontroller arm-client-controller
 else
   if [ "$1" == "all" ]
   then
     kubectl delete replicationcontroller rabbitmq-controller
-    kubectl delete replicationcontroller mcv-controller
-    kubectl delete replicationcontroller mcv-parent-controller
-    kubectl delete replicationcontroller mcv-client-controller
+    kubectl delete replicationcontroller intel-worker-controller
+    kubectl delete replicationcontroller intel-splitter-controller
+    kubectl delete replicationcontroller intel-client-controller
+    kubectl delete replicationcontroller arm-worker-controller
+    kubectl delete replicationcontroller arm-splitter-controller
+    kubectl delete replicationcontroller arm-client-controller
     kubectl delete replicationcontroller splunk-controller
     kubectl delete service loadbalancer
     kubectl delete service rabbitmq-service
