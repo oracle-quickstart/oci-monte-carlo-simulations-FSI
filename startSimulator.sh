6ecbeb72d47707d29f76e6fe534a76c86e4eeaf7
joaquinspereira <86013068+joaquinspereira@users.noreply.github.com>
2022-01-31 16:09:04 +0100

    Update startSimulator.sh

--- startSimulator.sh
+++ startSimulator.sh
@@ -9,26 +9,26 @@
 
 echo "Deploying OCI FSI Monte Carlo Simulator"
 
-kubectl create -f ks8-deployment/all_architectures/storageClass.yaml
-kubectl create -f ks8-deployment/all_architectures/persistentVolume.yaml
-kubectl create -f ks8-deployment/all_architectures/persistentVolumeClaim.yaml
-kubectl create -f ks8-deployment/all_architectures/splunk-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/splunk-service.yaml
+kubectl create -f ks8-deployment/storageClass.yaml
+kubectl create -f ks8-deployment/persistentVolume.yaml
+kubectl create -f ks8-deployment/persistentVolumeClaim.yaml
+kubectl create -f ks8-deployment/splunk-controller.yaml
+kubectl create -f ks8-deployment/splunk-service.yaml
 echo "Reporting tool deployed"
 
-kubectl create -f ks8-deployment/all_architectures/rabbitmq-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/rabbitmq-service.yaml
+kubectl create -f ks8-deployment/rabbitmq-controller.yaml
+kubectl create -f ks8-deployment/rabbitmq-service.yaml
 echo "RabbitMQ deployed"
-kubectl create -f ks8-deployment/all_architectures/loadbalancer-service.yaml
+kubectl create -f ks8-deployment/loadbalancer-service.yaml
 echo "LoadBalancer deployed"
 sleep 15
 
-kubectl create -f ks8-deployment/all_architectures/intel-worker-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/intel-splitter-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/intel-client-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/arm-worker-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/arm-splitter-controller.yaml
-kubectl create -f ks8-deployment/all_architectures/arm-client-controller.yaml
+kubectl create -f ks8-deployment/intel-worker-controller.yaml
+kubectl create -f ks8-deployment/intel-splitter-controller.yaml
+kubectl create -f ks8-deployment/intel-client-controller.yaml
+kubectl create -f ks8-deployment/arm-worker-controller.yaml
+kubectl create -f ks8-deployment/arm-splitter-controller.yaml
+kubectl create -f ks8-deployment/arm-client-controller.yaml
 
 
 echo "Monte Carlo Simulator core deployed"
